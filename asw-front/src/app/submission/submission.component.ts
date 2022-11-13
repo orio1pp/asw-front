2aa8c9c3d542bc52833f89e0593dfd95caa897c1
AlejandroLozanoAparicio <101321786+AlejandroLozanoAparicio1@users.noreply.github.com>
2022-11-13 14:34:46 +0100

    coses de comments

--- asw-front/src/app/submission/submission.component.ts
+++ asw-front/src/app/submission/submission.component.ts
@@ -17,6 +17,8 @@ export class SubmissionComponent implements OnInit {
 
   likeClass: string = 'not-liked';
   points: number = 0;
+  likesPoints: [] = [];
+  commentPoints: [] = [];
 
   constructor(
     private submissionControllerComponent: SubmissionControllerComponent
@@ -73,47 +75,85 @@ export class SubmissionComponent implements OnInit {
     let dynamicTemplate = `
     <div>
       <div>[SUPERCHRIS]</div>
-    </div>
-  `;
-    // let idNews: string | null = localStorage.getItem('idNews');
-    let idNews = '53';
+    </div>`;
+
+    let idNews: string | null = localStorage.getItem('submission');
     let commentaries;
-    await this.submissionControllerComponent
-      .getCommentaries(idNews)
-      .then((data) => {
-        commentaries = data;
-      });
-    commentaries = commentaries as unknown as Comments[];
-    for (let i = commentaries.length - 1; i > -1; --i) {
-      let comment;
+    if (idNews !== null) {
       await this.submissionControllerComponent
-        .getComments(commentaries[i].id.toString())
+        .getCommentaries(idNews)
         .then((data) => {
-          comment = data;
+          commentaries = data;
         });
-      this.getHtml(comment as unknown as Comments);
+      commentaries = commentaries as unknown as Comments[];
+      for (let i = commentaries.length - 1; i > -1; --i) {
+        let comment;
+        await this.submissionControllerComponent
+          .getComments(commentaries[i].id.toString())
+          .then((data) => {
+            comment = data;
+          });
+        this.getHtml(comment as unknown as Comments, 0);
+      }
+      dynamicTemplate.replace('[SUPERCHRIS]', this.commentsection);
     }
-    dynamicTemplate.replace('[SUPERCHRIS]', this.commentsection);
     return dynamicTemplate;
   }
 
-  private getHtml(comment: Comments) {
+  private getHtml(comment: Comments, margin: number) {
+    const left = 'com' + margin.toString();
+    const rep = 'rep' + comment.id.toString();
+    console.log(rep);
     if (comment.replies.length == 0) {
-      this.commentsection += `<div class="reply comment">
+      this.commentsection += `<div class="comment ${left}">
+                                <div class="comment-info">
+                                  <div
+                                    id="${comment.id}"
+                                    alt="heart"
+                                    #elem
+                                    (click)="likeBtn(elem.id)"
+                                    class="{{ likeClass.get(comment.id) }}"
+                                  ></div>
+                                  <p class="comment-points">${
+                                    this.commentPoints[comment.id]
+                                  } points</p>
+                                  <p class="comment-user">
+                                    by <span>${comment.user.username}</span>
+                                  </p>
+                                  <p class="comment-date">at ${comment.time}</p>
+                                </div>
+                                <div class="comment-body">
+                                  <p class="comment-text">${comment.body}</p>
+                                </div>
+                                <div class="comment-reply">
+                                  <a
+                                    id=${rep}
+                                    #elem
+                                    class="reply-btn"
+                                    (click)="goToReply(elem.id)"
+                                    >reply
+                                  </a>
+                                </div>
+                              </div>`;
+
+      /*`
+                          <div class="${left}">
+                            <div class="reply comment">
                                 <div class="comment-info">
                                     <p class="comment-points">0 points ${comment.id}</p>
-                                    <p class="comment-user"> by ${comment.user.username}</p>
+                                    <p class="comment-user"> by <span class="comment-user-span">${comment.user.username}</span></p>
                                     <p class="comment-date"> at ${comment.time}</p>
                                 </div>
                                     <div class="comment-body">
-                                        <p class="comment-date">${comment.body}</p>
+                                        <p>${comment.body}</p>
                                     </div>
                                 </div>
-                            </div>`;
+                            </div>
+                          </div>`;*/
     }
 
     for (let i = 0; comment.replies.length > i; i++) {
-      this.getHtml(comment.replies[i]);
+      this.getHtml(comment.replies[i], margin + 3);
     }
   }
 
