b8c8f8a044a601fa92ab611134e7f15336788b29
AlejandroLozanoAparicio <alejandro.lozano.aparicio@estudiantat.upc.edu>
2022-12-22 22:10:30 +0100

    voted submissions working

--- asw-front/src/app/voted-submissions-view/voted-submissions-view.component.ts
+++ asw-front/src/app/voted-submissions-view/voted-submissions-view.component.ts
@@ -1,26 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { News } from '../modelos/News';
 import { ProfileControllerComponent } from '../profile-controller/profile-controller.component';
 @Component({
   selector: 'app-voted-submissions-view',
   templateUrl: './voted-submissions-view.component.html',
-  styleUrls: ['./voted-submissions-view.component.css']
+  styleUrls: ['./voted-submissions-view.component.css'],
 })
 export class VotedSubmissionsViewComponent implements OnInit {
-
-  constructor(private profileControllerComponent:ProfileControllerComponent) { }
+  constructor(
+    private profileControllerComponent: ProfileControllerComponent,
+    private router: Router
+  ) {}
   news: News[] = [];
   ngOnInit(): void {
-    this.getLikedSubmissions()
+    this.getLikedSubmissions();
   }
 
-  public getLikedSubmissions(){
-    let username = localStorage.getItem("username");
-    username = "oriolportellpareras"
-    this.profileControllerComponent.getLikedSubmissions(username).then(data =>{
-      this.news = data
-      console.log(data)
-    })
+  public getLikedSubmissions() {
+    let username = localStorage.getItem('username') as string;
+    this.profileControllerComponent
+      .getLikedSubmissions(username)
+      .then((data) => {
+        this.news = data;
+      });
   }
 
+  goToSubmission(id: string): void {
+    localStorage.setItem('submission', id);
+    this.router.navigate(['submission']);
+  }
 }
